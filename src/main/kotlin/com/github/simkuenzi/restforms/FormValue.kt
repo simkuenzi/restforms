45cf284c63b4123920f69a8181dc2b794443f26a
Simon Künzi <sim.kuenzi@gmail.com>
2020-06-24 15:38:00 +0200

    Allow access to name of form value. Allows applications to implement their own field classes.

--- src/main/kotlin/com/github/simkuenzi/restforms/FormValue.kt
+++ src/main/kotlin/com/github/simkuenzi/restforms/FormValue.kt
@@ -4,4 +4,5 @@ import javax.ws.rs.core.MultivaluedMap
 
 class FormValue(internal val name: String, private val rawForm: MultivaluedMap<String, String>) {
     fun rawValue() = if (rawForm.containsKey(name)) rawForm.getFirst(name) else ""
+    fun name() = name
 }
